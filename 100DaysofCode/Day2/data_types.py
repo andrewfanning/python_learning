feb14e32e1f54a70047781e3b4d22190da0d1b9b
Andrew <andrew.fanning@gmail.com>
2022-04-20 13:14:30 -0500

    Day 2 Submission

---
+++ 100DaysofCode/Day2/data_types.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#First *fork* your copy. Then copy-paste your code below this line 👇
+#Finally click "Run" to execute the tests
+
+result = int(two_digit_number[0]) + int(two_digit_number[1])
+
+print(result)
